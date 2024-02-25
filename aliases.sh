efcc6ea743d00cb14aa0fdceef7934e47a7f99ac
Nicolas1st <nicolashereigo@gmail.com>
2024-02-25 13:25:12 +0300

    Add aliases

---
+++ aliases.sh
@@ -0,0 +1,12 @@
+alias gs="git status"
+alias ga="git add"
+alias gl="git log"
+alias gcm="git commit --amend"
+alias gc="git checkout"
+alias gr="git reset"
+alias gds="git diff --staged"
+alias gb="git branch";
+commit() {
+    git commit -m "$@"
+}
+alias c="clear"
