9d1bc7f72f8fa1a486f9eaf9416524114ec1568c
Nicolas1st <nicolashereigo@gmail.com>
2024-03-03 17:00:47 +0300

    New aliases

--- aliases.sh
+++ aliases.sh
@@ -5,7 +5,9 @@ alias gcm="git commit --amend"
 alias gc="git checkout"
 alias gr="git reset"
 alias gds="git diff --staged"
+alias gd="git diff"
 alias gb="git branch";
+alias gc="git branch";
 commit() {
     git commit -m "$@"
 }
