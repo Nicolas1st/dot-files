9d1ccc230b08e3c4de329aca5b6907b5eabb9c94
Nicolas1st <nicolashereigo@gmail.com>
2024-03-03 17:04:17 +0300

    Update bash config

--- bash_configuration.sh
+++ bash_configuration.sh
@@ -1,3 +1,26 @@
+HISTTIMEFORMAT="%F %T: "
+
+get_current_branch_prefix() {
+    local branch_name="$(git symbolic-ref --short -q HEAD 2> /dev/null)"
+    local red='\033[0;31m'
+    local reset_color='\033[0m'
+
+    if [ -n "$branch_name" ]
+    then
+        echo -e "$red($branch_name)$reset_color "
+    fi
+}
+
+cdg() {
+  while ! [ -d .git ]
+  do
+      cd ..
+  done
+}
+
+export LC_ALL=C
+alias j="jobs -l"
+
 alias gs="git status"
 alias ga="git add"
 alias gl="git log"
